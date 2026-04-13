2f047c84202e7316ee3c6bee6e5fe27f4c2000e5
Roy <royfrey@outlook.com>
2026-04-13 16:01:02 -0300

    Actualizacion 13-04-2026, boton ver proyectos actualizado

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -83,7 +83,7 @@ export default function Contact() {
                     placeholder="¿En qué puedo ayudarte?"
                     ></textarea>
             </div>
-            <button type="submit" className="bg-cyan-500 text-slate-800 px-6 py-3 font-medium rounded-lg hover-wave transition-all">
+            <button type="submit" className="w-full bg-cyan-500 border-slate-700 text-slate-300 px-6 py-3 font-medium rounded-lg hover-wave transition-all">
                     Enviar Mensaje
             </button>
             </form>

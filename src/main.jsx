68babd280c36f04a34cc08c6b300460b112b16de
Roy <royfrey@outlook.com>
2026-04-20 15:01:07 -0300

    Actualizacion 20/4/2026, importando Analytics de Vercel para visualizar las visitas de la pagina

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { analytics } from '@vercel/analytics';
 import './index.css'
 import  i18n from './i18n'; // Importamos la config de diccionario de lenguaje
 import App from './App.jsx'

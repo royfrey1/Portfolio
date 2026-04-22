d7fd67e0c88d143a73389fa88e75975565015f53
Roy <royfrey@outlook.com>
2026-04-22 11:52:31 -0300

    fix: agregar dependencia de vercel analytics faltante

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { analytics } from '@vercel/analytics';
+import { inject } from '@vercel/analytics';
 import './index.css'
 import  i18n from './i18n'; // Importamos la config de diccionario de lenguaje
 import App from './App.jsx'
-
+inject();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />

0509c7322d6e3651cb992684678bbe87aa672e06
Roy <royfrey@outlook.com>
2026-05-19 12:54:06 -0300

    Agregando vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { inject } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import  i18n from './i18n'; // Importamos la config de diccionario de lenguaje
 import App from './App.jsx'
-inject();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
